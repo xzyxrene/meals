7374ae0664f680eba532fa8299fb357785dd5b96
xzyxrene <rivamraj@gmail.com>
2023-04-14 17:33:23 +0530

    favourite icon feedback updated

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -14,6 +14,10 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favouriteMealsProvider);
+
+    final isFavourite = favouriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -32,7 +36,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
